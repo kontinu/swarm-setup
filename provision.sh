32091e89e33a9b6755466914f3067ac21223ec4c
@jmarcos-cano <jmarcos.cano@gmail.com>
2020-08-26 17:41:46 -0600

    @jmarcos-cano - remove portainer

--- provision.sh
+++ provision.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 
 docker stack deploy -c <(docker-compose  -f traefik-stack.yml config ) traefik
@@ -8,6 +8,8 @@ docker stack deploy -c <(docker-compose  -f visualizer-stack.yml config ) viz
 
 
 # monitoring
-docker stack deploy -c docker-elk/docker-stack.yml -c elk.override.yml elk
+#docker stack deploy -c docker-elk/docker-stack.yml -c elk.override.yml elk
+
+# TODO: set vars
 docker stack deploy -c swarmprom/docker-compose.yml prom
-docker stack deploy -c stack-portainer.yml portainer
+# docker stack deploy -c stack-portainer.yml portainer
